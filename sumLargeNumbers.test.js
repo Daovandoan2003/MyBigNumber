69237490ee631efb9bf726f2ef85c48d18eed674
Daovandoan2003 <daovandoan2k03@andoan2k03gmail.com>
2025-03-29 21:01:22 +0700

    lần 1

---
+++ sumLargeNumbers.test.js
@@ -0,0 +1,25 @@
+const sumLargeNumbers = require("./sumLargeNumbers");
+
+describe("sumLargeNumbers", () => {
+  test("Cộng hai số nhỏ", () => {
+    expect(sumLargeNumbers("123", "456")).toBe("579");
+  });
+
+  test("Cộng hai số lớn", () => {
+    expect(sumLargeNumbers("987654321987654321", "123456789123456789")).toBe(
+      "1111111111111111110"
+    );
+  });
+
+  test("Cộng hai số có độ dài khác nhau", () => {
+    expect(sumLargeNumbers("999", "1")).toBe("1000");
+  });
+
+  test("Cộng số 0 với số khác", () => {
+    expect(sumLargeNumbers("0", "9999")).toBe("9999");
+  });
+
+  test("Cộng hai số 0", () => {
+    expect(sumLargeNumbers("0", "0")).toBe("0");
+  });
+});
